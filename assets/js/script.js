cc31f3566df39f1c7c996f8c8df0b3e8ecf029b3
Sharaf034 <sharafet@code.edu.az>
2023-09-13 22:25:43 +0400

    Add file

---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+"use strict"
+
+
+
+// let elem = document.getElementsByTagName("h1")
+
+// let elem = document.getElementsByClassName("Test")
+
+// let elem = document.getElementById("head")
+
+// let elem = document.querySelector("#head")
+
+// let elems = document.querySelectorAll("h1")
+
+// let span = document.querySelector(".header .header-item span")
+
+// let elems = document.querySelectorAll(".header p")
+
+// span.innerHTML = "snbfdbhbgduvs"
+
+// let elem = document.getElementById("text")
+// console.log(elem.innerText);
+
+// elems.forEach(element => {
+//     console.log(element);
+// });
+
+// for (const item of elems) {
+//     console.log(item);
+// }
+
+// for (let i = 0; i < elems.length; i++) {
+//    if(elems[i].innerText == "salam"){
+//     console.log("yes")--------elems[i].style.backgroundColor = ("green")
+//    }else{
+//     console.log("no")-------elems[i].style.backgroundColor = ("red")
+//    }
+// }
+
+
+// let elems = document.querySelector("h1")
+
+// for (const elem of elems) {
+//     elem.style.color = "magenta"
+//     elem.style.marginTop = "100px"
+// }
+
+// elems[0].style.color = "magenta"
+
+// let elem = document.getElementById("head")
+
+// elem.innerHTML = "<a> salamlar millet </a>"
+// elem.innerText = "<a> salamlar millet </a>"
+
+// elem.className = "header-color"
+
+// elem.classList.add("header-color");
+// elem.classList.add("mx-5");
+// elem.classList.remove("mt-5")
+
+// let button = document.querySelector("button");
+
+// if(button.classList.contains("btn-primary")){
+//     button.classList.removeq("btn-primary");
+//     button.classList.add("btn-success")
+// }else{
+//     button.classList.add("btn-warning")
+// }
+
+
+// let button = document.querySelector("button");
+
+// button.onclick = function(){
+//     console.log("Hello world1");
+// }
+
+// button.onclick = function(){
+//     console.log("Hello world2");
+// }
+
+// button.addEventListener("click", function(){
+//     console.log(":Hello world1");
+// })
+
+// button.addEventListener("click", function(){
+//     console.log(":Hello world2");
+// })
+
+// button.addEventListener("click", function(){
+//     console.log(":Hello world2");
+// })
+
+
+// let btnOn = document.querySelector(".buttons button:nth-child(1)");
+// let btnOff = document.querySelector(".buttons button:nth-child(2)");
+// let body = document.querySelector("body");
+
+
+// btnOn.addEventListener("click", function(){
+//     if(!body.classList.contains("body-on")){
+//         body.classList.add("body-on");
+//         body.classList.remove("body-off")
+//     }
+// })
+
+// btnOff.addEventListener("click", function(){
+//     if(!body.classList.contains("body-off")){
+//         body.classList.add("body-off");
+//         body.classList.remove("body-on")
+//     }
+// })
+
+
+// let body = document.querySelector("body");
+// let button = document.querySelector(".buttons button");
+
+// button.addEventListener("click", function(){
+//     if(body.classList.contains("body-off")){
+//         body.classList.remove("body-off")
+//         this.innerText("On")
+//     }else{
+//         body.classList.add("body-off")
+//         this.innerText("Off")
+//     }
+// })
